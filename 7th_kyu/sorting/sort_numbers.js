bd7863b7ad71362f989065fb959368545ae85785
Akil Lutes <lutes.akil@gmail.com>
2022-04-18 21:55:54 -0400

    sort_numbers

---
+++ 7th_kyu/sorting/sort_numbers.js
@@ -0,0 +1,24 @@
+/* 
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+*/
+
+
+
+
+// 4/18/2022
+// function solution(nums){
+//     // I need to use sort(a,b) => a-b formula
+//     const nullArr = []
+//     if (nums === null) {
+//         return nullArr
+//     }
+//     return nums.sort((a, b) => a - b)
+// }
+
+// console.log(solution([1, 2, 10, 50, 5]));
+// console.log(solution([null]));

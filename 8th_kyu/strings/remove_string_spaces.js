8bb78dbaf5ca3eecbeb0c630967a473369dcc08b
Akil Lutes <lutes.akil@gmail.com>
2022-05-03 20:48:40 -0400

    remove string spaces

---
+++ 8th_kyu/strings/remove_string_spaces.js
@@ -0,0 +1,31 @@
+/*
+Simple, remove the spaces from the string, then return the resultant string.
+
+https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *** 5/3/22 ***
+// function noSpace(x){
+//     return x.split(' ').join('')
+// }
+
+// console.log(noSpace('Mindset thing'));

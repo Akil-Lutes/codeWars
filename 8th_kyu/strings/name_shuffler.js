9c821294480fab7d410da7c6a82290a79f2629ff
Akil Lutes <lutes.akil@gmail.com>
2022-07-07 18:29:41 -0400

    remove string spaces

--- 8th_kyu/strings/name_shuffler.js
+++ 8th_kyu/strings/name_shuffler.js
@@ -2,6 +2,8 @@
 Write a function that returns a string in which firstname is swapped with last name.
 
 nameShuffler('john McClane'); => "McClane john"
+
+https://www.codewars.com/kata/559ac78160f0be07c200005a
 */
 
 
--- 8th_kyu/strings/remove_string_spaces.js
+++ 8th_kyu/strings/remove_string_spaces.js
@@ -5,11 +5,12 @@ https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
 */
 
 
+// 7/7/2022
+function noSpace(x){
+    return x.split(' ').join('')
+}
 
-
-
-
-
+console.log(noSpace('A mindset thing'))
 
 
 
@@ -24,8 +25,8 @@ https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
 
 
 // *** 5/3/22 ***
-function noSpace(x){
-    return x.split(' ').join('')
-}
+// function noSpace(x){
+//     return x.split(' ').join('')
+// }
 
-console.log(noSpace('Mindset thing'));
+// console.log(noSpace('Mindset thing'));

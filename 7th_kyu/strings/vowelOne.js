1db236f97ea4da0c59f4a4887ad5184d49ebca8f
Akil Lutes <lutes.akil@gmail.com>
2023-04-15 16:15:50 -0400

    vowelOne practice

--- 7th_kyu/strings/vowelOne.js
+++ 7th_kyu/strings/vowelOne.js
@@ -1,4 +1,7 @@
 /* 
+
+https://www.codewars.com/kata/580751a40b5a777a200000a1/train/javascript
+
 Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
 
 All non-vowels including non alpha characters (spaces,commas etc.) should be included.
@@ -11,25 +14,46 @@ vowelOne( "aeiou, abc" ) // "1111100100"
 
 */
 
-function vowelOne(s) {
-    // ...
-    let empStr = [];
-    // Need Vowel Array
-    const vowArr = ['a', 'e', 'i', 'o', 'u'];
-    // I might need to convert a string to an array and back to a string
-    const strArr = s.split('');
-    // Need a for loop
-    // Need a for loop for vowels for matching
-    // Can use reduce method
-    // I need to put out the matching strArr vowArr vowels for matching
-    for (let i = 0; i < strArr.length; i++) {
-        for (let j = 0; j < vowArr.length; j++) {
-            if (vowArr[j] === strArr[i]) {
-                empStr.push(1)
-            }
-        }
+
+// 4/15/23
+function vowelOne(s){
+    s = s.toLowerCase()
+    const arr = ['a', 'e', 'i', 'o', 'u']
+    let result = ''
+    for (el of s) {
+        // ternary operator
+      arr.includes(el) ? result += '1' : result += '0'
     }
-    console.log(empStr)
-}
+    return result
+  }
+
+
+
+
+// 4/13/23 (First attempt)
+// function vowelOne(s) {
+//     // ...
+//     let empStr = [];
+//     // Need Vowel Array
+//     const vowArr = ['a', 'e', 'i', 'o', 'u'];
+//     // I might need to convert a string to an array and back to a string
+//     const strArr = s.split('');
+//     // Need a for loop
+//     // Need a for loop for vowels for matching
+//     // Can use reduce method
+//     // I need to put out the matching strArr vowArr vowels for matching
+//     for (let i = 0; i < strArr.length; i++) {
+//         for (let j = 0; j < vowArr.length; j++) {
+//             if (vowArr[j] === strArr[i]) {
+//                 let temp = vowArr.shift();
+//                 empStr.push(1)
+//             }
+//             if (vowArr[j] != strArr[i]) {
+//                 let tempCon = strArr.shift();
+//             }
+//         }
+//     }
+//     console.log(empStr)
+// }
   
-vowelOne('Manon, Guile');
+// vowelOne('Manon, Guile');
